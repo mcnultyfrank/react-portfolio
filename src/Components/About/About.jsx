e8345a8a4a1cd66773668ff77ad4409132bffc96
Frank McNulty <f-mcnulty@hotmail.com>
2021-01-14 11:26:34 +0000

    typos fixes

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -14,16 +14,16 @@ const About = () => {
         <h2>Hey <span className={styles.wave}> 👋</span></h2>
         <div>
             <p>
-                I'm a Software Developer and a graduate from _nology, where I completed a full time 12-week course. 
+                I'm a Software Developer and a graduate from _nology, where I completed a full-time 12-week course. 
                 <br></br>
                 <br></br>
                 I thoroughly enjoy creating compelling designs by following modern UI and UX design principles and applying them to my projects. 
                 <br></br>
                 <br></br>
-                I'm most productive when i'm working with others, and have experience working within in a Srum using the Agile framework, where we worked together to deliver our product weekly to our client.
+                I'm most productive when I'm working with others, and have experience working within in a Scrum using the Agile framework, where we worked together to deliver our product weekly to our client.
                 <br></br>
                 <br></br>
-                I'm always looking to expand my repertoire of technologies. I'm currently learning about servless technology and the cloud.                  
+                I'm always looking to expand my repertoire of technologies. I'm currently learning about serverless technology and the cloud.                  
                 </p>
                 <img src= {me} alt="" srcset=""/>
 
@@ -31,13 +31,15 @@ const About = () => {
           <div>
                 <img src= {india} alt="" srcset=""/>
                 <div>
-                  <p>I love to travel and in 2019 went exploring around India and Nepal for 6 months.</p>
+                  <p>
+                    I love to travel and in 2019 went exploring around India and Nepal for 6 months.
+                    </p>
                   <br></br>
                   <br></br>
-                  <p>I'm also university graduate and have a 2:1 BSC from The University Of West of England in Audio and Music Technology, where I learned to develop electronic synthesisers in C and C++.</p>
+                  <p>I'm also a University graduate and have a 2:1 BSC from The University Of West of England in Audio and Music Technology, where I learned to develop electronic synthesisers in C and C++.</p>
                   <br></br>
                   <br></br>
-                  <p>As well as coding, i'm absolutely obsessed about music and produce electronic music in Ableton Live, and DJ as a hobby on my Technics and  CDJs.</p>
+                  <p>As well as coding, I'm obsessed about music and produce electronic music in Ableton Live, and DJ as a hobby on my Technics and  CDJs.</p>
                 </div>
           </div>
           <div>
